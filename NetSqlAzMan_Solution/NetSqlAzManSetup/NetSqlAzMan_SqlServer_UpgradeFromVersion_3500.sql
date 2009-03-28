a70aaf0f8c04a090b0076c8721b45247448371d4
SND\aferende_cp <SND\aferende_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2009-03-28 10:39:28 +0000

    3.5.3.0 - alpha 1 - NetSqlAzMan Storage script (T-SQL) updated
    git-svn-id: https://netsqlazman.svn.codeplex.com/svn@20198 9b283d60-5439-405e-af05-b73fd8c4d996

--- NetSqlAzMan_Solution/NetSqlAzManSetup/NetSqlAzMan_SqlServer_UpgradeFromVersion_3500.sql
+++ NetSqlAzMan_Solution/NetSqlAzManSetup/NetSqlAzMan_SqlServer_UpgradeFromVersion_3500.sql
@@ -1,6 +1,6 @@
 --
 -- Script To Update dbo.NetSqlAzMan_DBVersion Function In ..NetSqlAzMan_3500
--- Generated sabato, aprile 12, 2008, at 02.35 PM
+-- Generated sabato, marzo 28, 2009, at 11.35 AM
 --
 -- Please backup ..NetSqlAzMan_3500 before executing this script
 --
@@ -60,5 +60,174 @@ BEGIN
 END ELSE
 BEGIN
    PRINT 'Failed To Update dbo.NetSqlAzMan_DBVersion Function'
+END
+GO
+
+--
+-- Script To Update dbo.ExecuteLDAPQuery Procedure In ..NetSqlAzMan_3500
+-- Generated sabato, marzo 28, 2009, at 11.35 AM
+--
+-- Please backup ..NetSqlAzMan_3500 before executing this script
+--
+
+
+BEGIN TRANSACTION
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+
+PRINT 'Updating dbo.ExecuteLDAPQuery Procedure'
+GO
+
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, QUOTED_IDENTIFIER, CONCAT_NULL_YIELDS_NULL ON
+GO
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+
+
+exec('ALTER PROCEDURE [dbo].[ExecuteLDAPQuery](@LDAPPATH NVARCHAR(4000), @LDAPQUERY NVARCHAR(4000), @members_cur CURSOR VARYING OUTPUT)
+AS
+-- REMEMBER !!!
+-- BEFORE executing ExecuteLDAPQuery procedure ... a Linked Server named ''ADSI'' must be added:
+-- --sp_addlinkedserver ''ADSI'', ''Active Directory Service Interfaces'', ''ADSDSOObject'', ''adsdatasource''
+CREATE TABLE #temp (objectSid VARBINARY(85))
+IF @LDAPQUERY IS NULL OR RTRIM(LTRIM(@LDAPQUERY))='''' OR @LDAPPATH IS NULL OR RTRIM(LTRIM(@LDAPPATH))=''''
+BEGIN
+SET @members_cur = CURSOR STATIC FORWARD_ONLY FOR SELECT * FROM #temp
+OPEN @members_cur
+DROP TABLE #temp
+RETURN
+END
+SET @LDAPPATH = REPLACE(@LDAPPATH, N'''''''', N'''''''''''')
+SET @LDAPQUERY = REPLACE(@LDAPQUERY, N'''''''', N'''''''''''')
+DECLARE @QUERY nvarchar(4000)
+DECLARE @LDAPROOTDSEPART nvarchar(4000)
+DECLARE @LDAPQUERYPART nvarchar(4000)
+SET @LDAPROOTDSEPART = LTRIM(@LDAPQUERY)
+IF CHARINDEX(''[RootDSE:'', @LDAPROOTDSEPART)=1
+BEGIN
+	SET @LDAPROOTDSEPART = SUBSTRING(@LDAPROOTDSEPART, 10, CHARINDEX('']'', @LDAPROOTDSEPART)-10)
+	SET @LDAPQUERYPART = SUBSTRING(@LDAPQUERY, CHARINDEX( '']'', @LDAPQUERY)+1, 4000)
+END
+ELSE
+BEGIN
+	SET @LDAPROOTDSEPART = @LDAPPATH
+	SET @LDAPQUERYPART = @LDAPQUERY
+END
+SET @QUERY = CHAR(39) + ''<'' + ''LDAP://''+ @LDAPROOTDSEPART + ''>;(&(!(objectClass=computer))(&(|(objectClass=user)(objectClass=group)))'' + @LDAPQUERYPART + '');objectSid;subtree'' + CHAR(39) 
+DECLARE @OPENQUERY nvarchar(4000)
+SET @OPENQUERY = ''SELECT * FROM OPENQUERY(ADSI, '' + @QUERY + '')''
+INSERT INTO #temp EXEC (@OPENQUERY)
+SET @members_cur = CURSOR STATIC FORWARD_ONLY FOR SELECT * FROM #temp
+OPEN @members_cur
+DROP TABLE #temp')
+GO
+
+IF @@ERROR <> 0
+   IF @@TRANCOUNT = 1 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT = 1
+BEGIN
+   PRINT 'dbo.ExecuteLDAPQuery Procedure Updated Successfully'
+   COMMIT TRANSACTION
+END ELSE
+BEGIN
+   PRINT 'Failed To Update dbo.ExecuteLDAPQuery Procedure'
+END
+GO
+
+--
+-- Script To Update dbo.helplogins Procedure In ..NetSqlAzMan_3500
+-- Generated sabato, marzo 28, 2009, at 11.35 AM
+--
+-- Please backup ..NetSqlAzMan_3500 before executing this script
+--
+
+
+BEGIN TRANSACTION
+GO
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
+GO
+
+PRINT 'Updating dbo.helplogins Procedure'
+GO
+
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, QUOTED_IDENTIFIER, CONCAT_NULL_YIELDS_NULL ON
+GO
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+
+REVOKE Execute ON [helplogins] TO [NetSqlAzMan_Readers]
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
+
+SET ANSI_NULLS OFF
+GO
+
+exec('ALTER PROCEDURE [dbo].[helplogins](@rolename nvarchar(128))
+AS
+
+CREATE TABLE #temptable (
+	[DBRole] sysname NOT NULL ,
+	[MemberName] sysname NOT NULL ,
+	[MemberSID] varbinary(85) NULL
+	)
+
+IF @rolename = ''NetSqlAzMan_Managers''
+BEGIN
+	INSERT INTO #temptable EXEC sp_helprolemember ''NetSqlAzMan_Managers''
+END
+
+IF @rolename = ''NetSqlAzMan_Users'' 
+BEGIN
+	INSERT INTO #temptable EXEC sp_helprolemember ''NetSqlAzMan_Managers''
+	INSERT INTO #temptable EXEC sp_helprolemember ''NetSqlAzMan_Users''
+END
+
+IF @rolename = ''NetSqlAzMan_Readers'' 
+BEGIN
+	INSERT INTO #temptable EXEC sp_helprolemember ''NetSqlAzMan_Managers''
+	INSERT INTO #temptable EXEC sp_helprolemember ''NetSqlAzMan_Users''
+	INSERT INTO #temptable EXEC sp_helprolemember ''NetSqlAzMan_Readers''
+END
+
+SELECT DISTINCT SUSER_SNAME(MemberSid) SqlUserOrRole, CASE MemberSid WHEN NULL THEN 1 ELSE 0 END AS IsSqlRole
+FROM #temptable
+WHERE MemberName NOT IN (''NetSqlAzMan_Administrators'', ''NetSqlAzMan_Managers'', ''NetSqlAzMan_Users'', ''NetSqlAzMan_Readers'')
+AND SUSER_SNAME(MemberSid) IS NOT NULL
+ORDER BY SUSER_SNAME(MemberSid)
+
+DROP TABLE #temptable')
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+IF @@ERROR <> 0
+   IF @@TRANCOUNT = 1 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT = 1
+   GRANT Execute ON [helplogins] TO [NetSqlAzMan_Readers]
+GO
+
+IF @@ERROR <> 0
+   IF @@TRANCOUNT = 1 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT = 1
+BEGIN
+   PRINT 'dbo.helplogins Procedure Updated Successfully'
+   COMMIT TRANSACTION
+END ELSE
+BEGIN
+   PRINT 'Failed To Update dbo.helplogins Procedure'
 END
 GO
--- NetSqlAzMan_Solution/NetSqlAzManSetup/NetSqlAzMan_SqlServer_UpgradeFromVersion_3520.sql
+++ NetSqlAzMan_Solution/NetSqlAzManSetup/NetSqlAzMan_SqlServer_UpgradeFromVersion_3520.sql
@@ -1,28 +1,8 @@
 --
--- Please backup ..NetSqlAzManStorage before executing this script
+-- Script To Update dbo.NetSqlAzMan_DBVersion Function In ..NetSqlAzMan_3520
+-- Generated sabato, marzo 28, 2009, at 11.37 AM
 --
-
-
-PRINT 'Creating dbo User'
-GO
-
-exec sp_revokedbaccess N''
-GO
-
-if not exists (select * from master.dbo.syslogins where loginname = N'sa')
-   exec sp_addlogin N'sa'
-GO
-
-exec sp_grantdbaccess N'sa', N'dbo'
-GO
-
-PRINT 'dbo User Updated Successfully'
-GO
-
---
--- Script To Update dbo.NetSqlAzMan_DBVersion Function In ..NetSqlAzMan
---
--- Please backup ... NetSqlAzManStorage before executing this script
+-- Please backup ..NetSqlAzMan_3520 before executing this script
 --
 
 
@@ -83,48 +63,77 @@ BEGIN
 END
 GO
 
-/****** Object:  StoredProcedure [dbo].[ExecuteLDAPQuery]    Script Date: 03/24/2009 22:02:00 ******/
-SET ANSI_NULLS ON
+--
+-- Script To Update dbo.ExecuteLDAPQuery Procedure In ..NetSqlAzMan_3520
+-- Generated sabato, marzo 28, 2009, at 11.37 AM
+--
+-- Please backup ..NetSqlAzMan_3520 before executing this script
+--
+
+
+BEGIN TRANSACTION
 GO
-SET QUOTED_IDENTIFIER ON
+SET TRANSACTION ISOLATION LEVEL SERIALIZABLE
 GO
 
-ALTER PROCEDURE [dbo].[ExecuteLDAPQuery](@LDAPPATH NVARCHAR(4000), @LDAPQUERY NVARCHAR(4000), @members_cur CURSOR VARYING OUTPUT)
+PRINT 'Updating dbo.ExecuteLDAPQuery Procedure'
+GO
+
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, QUOTED_IDENTIFIER, CONCAT_NULL_YIELDS_NULL ON
+GO
+
+SET NUMERIC_ROUNDABORT OFF
+GO
+
+
+exec('ALTER PROCEDURE [dbo].[ExecuteLDAPQuery](@LDAPPATH NVARCHAR(4000), @LDAPQUERY NVARCHAR(4000), @members_cur CURSOR VARYING OUTPUT)
 AS
 -- REMEMBER !!!
--- BEFORE executing ExecuteLDAPQuery procedure ... a Linked Server named 'ADSI' must be added:
--- --sp_addlinkedserver 'ADSI', 'Active Directory Service Interfaces', 'ADSDSOObject', 'adsdatasource'
+-- BEFORE executing ExecuteLDAPQuery procedure ... a Linked Server named ''ADSI'' must be added:
+-- --sp_addlinkedserver ''ADSI'', ''Active Directory Service Interfaces'', ''ADSDSOObject'', ''adsdatasource''
 CREATE TABLE #temp (objectSid VARBINARY(85))
-IF @LDAPQUERY IS NULL OR RTRIM(LTRIM(@LDAPQUERY))='' OR @LDAPPATH IS NULL OR RTRIM(LTRIM(@LDAPPATH))=''
+IF @LDAPQUERY IS NULL OR RTRIM(LTRIM(@LDAPQUERY))='''' OR @LDAPPATH IS NULL OR RTRIM(LTRIM(@LDAPPATH))=''''
 BEGIN
 SET @members_cur = CURSOR STATIC FORWARD_ONLY FOR SELECT * FROM #temp
 OPEN @members_cur
 DROP TABLE #temp
 RETURN
 END
-SET @LDAPPATH = REPLACE(@LDAPPATH, N'''', N'''''')
-SET @LDAPQUERY = REPLACE(@LDAPQUERY, N'''', N'''''')
+SET @LDAPPATH = REPLACE(@LDAPPATH, N'''''''', N'''''''''''')
+SET @LDAPQUERY = REPLACE(@LDAPQUERY, N'''''''', N'''''''''''')
 DECLARE @QUERY nvarchar(4000)
 DECLARE @LDAPROOTDSEPART nvarchar(4000)
 DECLARE @LDAPQUERYPART nvarchar(4000)
 SET @LDAPROOTDSEPART = LTRIM(@LDAPQUERY)
-IF CHARINDEX('[RootDSE:', @LDAPROOTDSEPART)=1
+IF CHARINDEX(''[RootDSE:'', @LDAPROOTDSEPART)=1
 BEGIN
-	SET @LDAPROOTDSEPART = SUBSTRING(@LDAPROOTDSEPART, 10, CHARINDEX(']', @LDAPROOTDSEPART)-10)
-	SET @LDAPQUERYPART = SUBSTRING(@LDAPQUERY, CHARINDEX( ']', @LDAPQUERY)+1, 4000)
+	SET @LDAPROOTDSEPART = SUBSTRING(@LDAPROOTDSEPART, 10, CHARINDEX('']'', @LDAPROOTDSEPART)-10)
+	SET @LDAPQUERYPART = SUBSTRING(@LDAPQUERY, CHARINDEX( '']'', @LDAPQUERY)+1, 4000)
 END
 ELSE
 BEGIN
 	SET @LDAPROOTDSEPART = @LDAPPATH
 	SET @LDAPQUERYPART = @LDAPQUERY
 END
-SET @QUERY = CHAR(39) + '<' + 'LDAP://'+ @LDAPROOTDSEPART + '>;(&(!(objectClass=computer))(&(|(objectClass=user)(objectClass=group)))' + @LDAPQUERYPART + ');objectSid;subtree' + CHAR(39) 
+SET @QUERY = CHAR(39) + ''<'' + ''LDAP://''+ @LDAPROOTDSEPART + ''>;(&(!(objectClass=computer))(&(|(objectClass=user)(objectClass=group)))'' + @LDAPQUERYPART + '');objectSid;subtree'' + CHAR(39) 
 DECLARE @OPENQUERY nvarchar(4000)
-SET @OPENQUERY = 'SELECT * FROM OPENQUERY(ADSI, ' + @QUERY + ')'
+SET @OPENQUERY = ''SELECT * FROM OPENQUERY(ADSI, '' + @QUERY + '')''
 INSERT INTO #temp EXEC (@OPENQUERY)
 SET @members_cur = CURSOR STATIC FORWARD_ONLY FOR SELECT * FROM #temp
 OPEN @members_cur
-DROP TABLE #temp
-
+DROP TABLE #temp')
 GO
 
+IF @@ERROR <> 0
+   IF @@TRANCOUNT = 1 ROLLBACK TRANSACTION
+GO
+
+IF @@TRANCOUNT = 1
+BEGIN
+   PRINT 'dbo.ExecuteLDAPQuery Procedure Updated Successfully'
+   COMMIT TRANSACTION
+END ELSE
+BEGIN
+   PRINT 'Failed To Update dbo.ExecuteLDAPQuery Procedure'
+END
+GO
