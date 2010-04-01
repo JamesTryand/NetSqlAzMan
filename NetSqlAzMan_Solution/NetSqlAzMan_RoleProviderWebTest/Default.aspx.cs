2b2bf785d26919e7d84bedd993f4314456165865
SND\aferende_cp <SND\aferende_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2010-04-01 07:01:29 +0000

    Add NetSqlAzMan_RoleProviderTest web project (internal testing).
    git-svn-id: https://netsqlazman.svn.codeplex.com/svn@46391 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ NetSqlAzMan_Solution/NetSqlAzMan_RoleProviderWebTest/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Reflection;
+
+namespace NetSqlAzMan_RoleProviderWebTest
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            bool isInRole = Roles.IsUserInRole("Utente2");
+        }
+    }
+}
